ffc3ba4907c7c6a7ee6d035a28dd43f1143b5c2f
karun <karun.raj@juspay.in>
2024-12-16 14:03:36 +0530

    test commit 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Default Starter - making test changes',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
