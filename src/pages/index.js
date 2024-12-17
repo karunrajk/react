6eac920a03fd20fc2e478f37d558304390d1eca8
karun <karun.raj@juspay.in>
2024-12-17 16:01:26 +0530

    Chapter 4 - Timeline Interactions & Animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
       <div className='HeroGroup'> 
 
       <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site123.</p>
+      <p>Welcome to <br /> the best site hahahahah.</p>
       <p>Now go build something great. Random text random text random text random text random text random text random text.</p>
       <Link to="/page-2/">just click here already</Link>
 
